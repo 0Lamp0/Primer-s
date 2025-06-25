b13716c96070cb2f7cfe9c753ced30fb68e2e66b
Lamp <aliburto228@gmail.com>
2025-06-25 16:00:16 +0500

    Первая дороботка проекта)

--- ConsoleApplication7.cpp
+++ ConsoleApplication7.cpp
@@ -1,89 +1,102 @@
-﻿#include <iostream>
-#include <string>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
-#include <sstream>
-#include <algorithm>
+﻿#include <iostream>     
+#include <string>       
+#include <ctime>        
+#include <cmath>        
+#include <muParser.h>   
 
 using namespace std;
 
-// Генерация случайного числа в диапазоне [min, max]
+// Генератор случайного целого числа от min до max включительно
 int randomInt(int min, int max) {
     return min + rand() % (max - min + 1);
 }
 
-// Генерация случайного выражения
-string generateExpression() {
-    vector<string> numbers = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-    vector<string> ops = { "+", "-", "*", "/" };
+// Генератор случайного арифметического знака
+char ops() {
+    char oper[] = { '+', '-', '*', '/' };
+    return oper[randomInt(0, 3)];
+}
 
-    string expr;
-    int length = randomInt(3, 6); // Длина выражения (3-6 элементов)
-    bool needNumber = true;
+// Настройка кол-ва опеоаций 
+int min_terms = 2; 
+int max_terms = 4; 
 
-    for (int i = 0; i < length; ++i) {
-        if (needNumber) {
-            expr += numbers[randomInt(0, 9)];
-            // С вероятностью 30% добавляем скобки
-            if (randomInt(0, 9) < 3 && i < length - 2) {
-                expr = "(" + expr;
-                expr += ops[randomInt(0, 3)];
-                expr += numbers[randomInt(0, 9)] + ")";
-                i += 2; // Пропускаем следующие шаги
-            }
-        }
-        else {
-            expr += ops[randomInt(0, 3)];
-        }
-        needNumber = !needNumber;
+string AddOneBracket(string expr) {
+
+    if (rand() % 10 >3) // 70% случаев — не вставляем скобки
+        return expr;
+
+    vector<int> opPositions;
+    for (int i = 1; i < expr.length() - 1; i++) {
+        if (expr[i] == '+' || expr[i] == '-' || expr[i] == '*' || expr[i] == '/')
+            opPositions.push_back(i);
     }
+
+    if (opPositions.empty()) return expr;
+
+    int opIndex = opPositions[randomInt(0, opPositions.size() - 1)];
+
+    expr.insert(opIndex - 1, "(");
+    expr.insert(opIndex + 3, ")");
 
     return expr;
 }
 
-// Вычисление выражения (упрощённо, лучше использовать muparser)
-double evaluateExpression(const string& expr) {
-    // Здесь должен быть парсер, но для примера используем "костыль"
-    // В реальном коде лучше подключить muparser или другой вычислитель
-    stringstream ss(expr);
-    double a, b;
-    char op;
-    ss >> a >> op >> b;
-    switch (op) {
-    case '+': return a + b;
-    case '-': return a - b;
-    case '*': return a * b;
-    case '/': return a / b;
-    default: return 0;
+//Генератор строки-примера
+string GenExpr() {
+    
+    string expr;
+    int LenExpr = 2*randomInt(min_terms, max_terms)+1;
+    char chekdel = 0;
+    bool needop = 1; 
+    for (int i = 0; i < LenExpr;i++)
+    {
+        if (needop)
+        {
+
+            if (chekdel != '/')
+            {
+                expr += to_string(randomInt(0, 9));
+
+            }
+            else
+                expr += to_string(randomInt(1, 9));
+        }
+        else 
+        {
+            chekdel = ops();
+            expr += chekdel;
+        
+        }
+        needop= !needop;
+
+        
+        
     }
+    return AddOneBracket(expr);
 }
 
+
+
+
+
+
+
+
 int main() {
-    setlocale(LC_ALL, "ru");
+    setlocale (LC_ALL, "ru");
     srand(time(0));
-    int n;
-    cout << "Сколько примеров решить? ";
-    cin >> n;
 
-    for (int i = 0; i < n; ++i) {
-        string expr = generateExpression();
-        double correctAnswer = evaluateExpression(expr);
-        double userAnswer;
+    for (int i=0; i < 10;i++)
+    cout << GenExpr() <<endl;
+    /*int numExamples;
+    
 
-        cout << "Пример: " << expr << endl;
-        do {
-            cout << "СКОЛЬКО БУДЕТ? ";
-            cin >> userAnswer;
-            if (abs(userAnswer - correctAnswer) < 1e-6) {
-                cout << "Правильно!" << endl;
-                break;
-            }
-            else {
-                cout << "Неправильно, попробуйте ещё раз." << endl;
-            }
-        } while (true);
-    }
+    cout << "Сколько примеров хотите решить? ";
+    cin >> numExamples; 
+    cout << "\n-------------------------------------------------\n- Используй (.) а не (,)\n- Точность выражения 6 символов после запятой\n------------------------------------------------";
 
+
+    cout << "Все примеры решены. Молодец!" << endl;*/
     return 0;
 }
